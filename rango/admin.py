634f848d329962b99386b7aa99b748053c6beea8
Edsterr <ethandunsmore@hotmail.com>
2017-01-21 16:10:24 +0000

    Removed excess files

--- rango/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from rango.models import Category, Page, UserProfile
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-    
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page)
-admin.site.register(UserProfile)
